11ff8e4c774d4edf1c78ab7621720986eb479715
Felipe Augusto Silva <1901281@SALAS.AULAS>
2019-10-15 09:10:30 -0300

    commit

--- javascript.js
+++ javascript.js
@@ -1,5 +1,22 @@
-function pegar(num){
-    var tela = document.getElementById("resultado");
-    tela.value = tela.value + num;
+var valor = 0;
+function calc(num){
+   document.getElementById("resultado").value += num;
+ }
+
+function calcule (){
+  var result = 0;
+  result = document.getElementById("resultado").value;
+  document.getElementById("resultado").value = eval(result);
 }
 
+function soma(){
+    valor += parseFloat(document.getElementById("resultado").value);
+}
+
+function subt(){
+    valor -= parseFloat(document.getElementById("resultado").value);
+}
+
+function acumulado(){
+    calc(valor);
+}
